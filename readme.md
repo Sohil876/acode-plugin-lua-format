793e6f8720dad8866b66630908a91f8579073928
Sohil876 <sohil876@gmail.com>
2023-01-10 19:22:05 +0530

    Version 1.0.0

--- readme.md
+++ readme.md
@@ -3,6 +3,10 @@
 Supports Lua 5.1 - 5.4, FiveM hashed strings & a touch of Luau\
 Using [luamin.js](https://github.com/Herrtt/luamin.js)
 
+### Installation
+
+For now just paste [this](https://raw.githubusercontent.com/Sohil876/acode-plugin-lua-format/main/) in `Acode > Settings > Plugins > Remote`
+
 ### License
 
 [MIT](https://choosealicense.com/licenses/mit/)
--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
 import plugin from "../plugin.json";
+const luamin = require("lua-format");
+
+const pluginId = plugin.id;
+const appSettings = acode.require("settings");
 
 const formatterSettings = {
     RenameVariables: false,
@@ -7,9 +11,92 @@ const formatterSettings = {
 };
 
 class AcodeLuaFormat {
-    async init() {}
+    worker = null;
 
-    async destroy() {}
+    constructor() {
+        this.run = this.run.bind(this);
+    }
+
+    static inferParser(filename) {
+        switch (filename.slice(filename.lastIndexOf(".") + 1)) {
+            case "lua":
+                return "lua";
+
+            default:
+                return null;
+        }
+    }
+
+    async init() {
+        const extensions = ["lua"];
+        acode.registerFormatter(pluginId, extensions, this.run);
+    }
+
+    async run() {
+        const { editor, activeFile } = editorManager;
+        const { session } = activeFile;
+        const code = editor.getValue();
+        const cursorPos = editor.getCursorPosition();
+        const parser = AcodeLuaFormat.inferParser(activeFile.name);
+        const cursorOptions = {
+            parser,
+            cursorOffset: this.#cursorPosTocursorOffset(cursorPos),
+            filepath: activeFile.name,
+            tabWidth: appSettings.value.tabSize,
+        };
+        const res = luamin.Beautify(code, formatterSettings);
+        this.#setValue(session, res);
+    }
+
+    async destroy() {
+        acode.unregisterFormatter(plugin.id);
+    }
+
+    #cursorPosTocursorOffset(cursorPos) {
+        let { row, column } = cursorPos;
+        const { editor } = editorManager;
+        const lines = editor.getValue().split("\n");
+        for (let i = 0; i < row - 1; i++) {
+            column += lines[i].length;
+        }
+        return column;
+    }
+
+    #cursorOffsetTocursorPos(cursorOffset) {
+        const { editor } = editorManager;
+        const lines = editor.getValue().split("\n");
+        let row = 0;
+        let column = 0;
+        for (let i = 0; i < lines.length; i++) {
+            if (column + lines[i].length >= cursorOffset) {
+                row = i;
+                column = cursorOffset - column;
+                break;
+            }
+            column += lines[i].length;
+        }
+        return {
+            row,
+            column,
+        };
+    }
+
+    #setValue(session, formattedCode) {
+        const { $undoStack, $redoStack, $rev, $mark } = Object.assign(
+            {},
+            session.getUndoManager()
+        );
+        session.setValue(formattedCode);
+        const undoManager = session.getUndoManager();
+        undoManager.$undoStack = $undoStack;
+        undoManager.$redoStack = $redoStack;
+        undoManager.$rev = $rev;
+        undoManager.$mark = $mark;
+        const { row, column } = this.#cursorOffsetTocursorPos(
+            formattedCode.cursorOffset
+        );
+        session.selection.moveCursorTo(row, column);
+    }
 }
 
 if (window.acode) {
