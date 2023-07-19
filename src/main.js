7aa4cf8b370783f922bce631274ba5b64ea63db4
Raunak Raj <71929976+bajrangCoder@users.noreply.github.com>
2023-07-19 13:11:59 +0530

    Fixed main.js to follow typings

--- src/main.js
+++ src/main.js
@@ -102,12 +102,12 @@ if (window.acode) {
     const acodePlugin = new AcodeLuaFormat();
     acode.setPluginInit(
         plugin.id,
-        (baseUrl, $page, { cacheFileUrl, cacheFile }) => {
+        async (baseUrl, $page, { cacheFileUrl, cacheFile }) => {
             if (!baseUrl.endsWith("/")) {
                 baseUrl += "/";
             }
             acodePlugin.baseUrl = baseUrl;
-            acodePlugin.init($page, cacheFile, cacheFileUrl);
+            await acodePlugin.init($page, cacheFile, cacheFileUrl);
         }
     );
     acode.setPluginUnmount(plugin.id, () => {
